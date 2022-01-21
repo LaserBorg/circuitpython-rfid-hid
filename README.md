0e4f5eaeded6be69d40e3a8475fbefa8281532f5
Philip Gutjahr <mail@philipgutjahr.de>
2022-01-21 12:25:48 +0100

    neopixel added

--- README.md
+++ README.md
@@ -1,20 +1,47 @@
 # circuitpython-rfid-hid
+![Raspberry Pi Pico with MFRC522-Module on Breadboard](doc/rfid-hid.jpg)
 convert RFID Tag Data into keystrokes by emulating a USB Keyboard on a Pi Pico, including leave-events.
-<br>It can read from and write to the tag, but currently only checks if the tag UID exists in a JSON file.
+<br>It can read and write tag data, but currently only checks if the tag UID exists in a JSON file.
 
 This Project could easily be used to build a Sound Toy that works similar to the popular Toniebox®.
 
-#### Hardware: 
+## Hardware: 
 - Raspberry Pi Pico (RP2040) with CircuitPython 7.x
 - MFRC522-based RFID reader using SPI
 - Mifare Classic 1k Tags (not sure if MFRC522 also supports different Standards..)
+- optional LED: APA106 (WS2811) or any other NeoPixel (WS2812)
 
 
 
-#### RFID reader
+
+### RFID (MFRC522 Module)
 RFID Class for [MFRC522](http://www.nxp.com/documents/data_sheet/MFRC522.pdf) is based on [domdfcoding/circuitpython-mfrc522](https://github.com/domdfcoding/circuitpython-mfrc522) (CircuitPython port),
 <br>forked from [wendlers/micropython-mfrc522](https://github.com/wendlers/micropython-mfrc522) (MicroPython port).
 
+| MFRC522   | Pico GPIO |
+|-----------|-----------|
+| sck       | GP06      |
+| mosi      | GP07      |
+| miso      | GP04      |
+| rst       | GP18      |
+| cs/sda    | GP05      |
+
+###Write Access to Pico's internal memory
+Write Access for Pico is enabled by grounding Pin GP02 to mass. This sets the USB-filesystem to *read-only*, allowing Pico to write to it's own memory. 
+Handled by ``boot.py`` at boot time as described [here](https://learn.adafruit.com/circuitpython-essentials/circuitpython-storage).
+
+| extra pins     | Pico GPIO |
+|----------------|-----------|
+| NeoPixel (PWM) | GP00      |
+ | write Access   | GP02      |
+
+### NeoPixel LED
+APA106 LED  is driven by [Adafruit CircuitPython library](https://github.com/adafruit/Adafruit_CircuitPython_NeoPixel) as described [here](https://learn.adafruit.com/getting-started-with-raspberry-pi-pico-circuitpython/neopixel-leds)
+and wired like this:
+![APA106 RGB LED](doc/WS2811_5mm.jpg)
+<br>[image source](https://www.espruino.com/WS2811)
+
+###USB HID (Keyboard Emulation)
 USB-HID utilizes [CircuitPython HID library](https://github.com/adafruit/Adafruit_CircuitPython_HID)
 which can emulate Keyboard and Mouse as described [here](https://learn.adafruit.com/circuitpython-essentials/circuitpython-hid-keyboard-and-mouse) or [here](https://blog.thestaticturtle.fr/getting-started-with-hid-and-the-pi-pico/).
 
@@ -22,17 +49,3 @@ which can emulate Keyboard and Mouse as described [here](https://learn.adafruit.
 ## Usage
 
 Copy ``code.py``, ``boot.py``, ``known_tags.json`` and the ``lib/`` directory with all it's content (``mfrc.py`` class and adafruit libraries) to the root of Pico's File System.
-
-Pins:
-
-| MFRC522 | Pico GPIO |
-|---------|-----------|
-| sck     | GP06      |
-| mosi    | GP07      |
-| miso    | GP04      |
-| rst     | GP18      |
-| cs/sda  | GP05      |
-
-
-Write Access for Pico is enabled by grounding Pin GP02 to mass. This sets the USB-filesystem to *read-only*, allowing Pico to write to it's own memory. 
-Handled by ``boot.py`` at boot time as described [here](https://learn.adafruit.com/circuitpython-essentials/circuitpython-storage).
--- code.py
+++ code.py
@@ -3,55 +3,60 @@ circuitpython-rfid-hid
 convert RFID Tag Data into keystrokes by emulating a USB Keyboard on a Pi Pico.
 https://github.com/LaserBorg/circuitpython-rfid-hid
 """
+
 import board
-import mfrc522
 import time
 import json
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+import neopixel
+import mfrc522
 
 
-# lookups
+# lookup for tag types
 tag_types = {0: "nothing",
              16: "MIFARE Classic 1K"}
 
-# define pins for MFRC522
+# Pins for MFRC522
 sck = board.GP6
 mosi = board.GP7
 miso = board.GP4 
 rst = board.GP18
 cs = board.GP5  # = sda
+# additional pins
+led_pin = board.GP0
 
-# sleep between rfid scans, in seconds 
-delay = 0.1
 
-# threshold how often *notfound* before leave-event, has to be >= 2
-notfound_limit = 3
-
+delay =          0.1  # sleep between rfid scans, in seconds
+notfound_limit = 4    # threshold how often *notfound* before leave-event, has to be >= 2
 
 # # TODO: try writing with process_rfid(write=True, adress=8, data=testdata)
 # testdata = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"
 
 
-# initialize mfrc522 with pins and delay and set antenna to maximum gain
-rdr = mfrc522.MFRC522(sck, mosi, miso, rst, cs, delay=int(delay*1000))
-rdr.set_antenna_gain(0x07 << 4)
-
-
-# # READ JSON
+# # READ known_tags from JSON
 # known_tags = {"0x4348b603": "WHITE CARD", "0x09a929b3": "BLUE TOKEN"}
-with open('known_tags.json', 'r') as file:
-    known_tags = json.load(file)
-
-# # TODO: WRITE JSON (or 'a' for APPEND)
-# with open('known_tags.json', 'w') as file:
-#     json.dump(known_tags, file)
+def get_known_tag_list(filename='known_tags.json'):
+    with open(filename, 'r') as file:
+        known_tags = json.load(file)
+    return known_tags
 
 
-# initialize Keyboard
-kbd = Keyboard(usb_hid.devices)
-keyboard = KeyboardLayoutUS(kbd)
+# TODO: WRITE new tags to JSON
+def dump_known_tag_list(known_tags):
+    with open('known_tags.json', 'w') as file:  # or use 'a' for append
+        json.dump(known_tags, file)
+
+
+def get_color_from_string(text):
+    if 'blue' in text.lower():
+        color = (0, 0, 255)
+    elif 'white' in text.lower():
+        color = (255, 255, 255)
+    else:
+        color = (255, 0, 0)
+    return color
 
 
 def process_rfid(write=False, adress=8, data=None):
@@ -100,33 +105,48 @@ def process_rfid(write=False, adress=8, data=None):
 
 
 if __name__ == "__main__":
+    # INIT
+    rdr = mfrc522.MFRC522(sck, mosi, miso, rst, cs, delay=int(delay * 1000))  # RFID
+    rdr.set_antenna_gain(0x07 << 4)
+
+    kbd = Keyboard(usb_hid.devices)  # Keyboard
+    keyboard = KeyboardLayoutUS(kbd)
+
+    led = neopixel.NeoPixel(led_pin, 1, pixel_order=neopixel.RGB)  # NeoPixel LED
+    led[0] = (0, 0, 0)
+
+    # read from known_tag JSON
+    known_tags = get_known_tag_list()
+
+    # state variables
     prev_uid = notfound = 0
+    
     print("ready..")
     
     while True:
         uid, payload = process_rfid(adress=8)
         
-        # NO CARD
+        # NO CARD FOUND
         if uid == 0:
             if uid != prev_uid and notfound >= notfound_limit:
                 prev_uid = notfound = 0
+                
                 print("leave")
                 keyboard.write("leave" + "\n")
-                
-            elif notfound >= notfound_limit:
-                # NO CARD FOUND
-                continue
+                led[0] = (0, 0, 0)
             
-            else:
+            elif notfound < notfound_limit:
                 notfound += 1
         
         else:
             if prev_uid == 0:
                 # CARD APPEARED
                 text = known_tags[uid] if uid in known_tags else uid
-                print(f"{text} appeared")
+                
+                print(text)
                 keyboard.write(text + "\n")
-            
+                led[0] = get_color_from_string(text)
+                 
             elif prev_uid == uid:
                 # CARD STILL THERE
                 time.sleep(delay)
